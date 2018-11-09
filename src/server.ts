e090bbc4cee97f08fb73a416cf78d1ce9053dc7c
Oliver Braun <ob@cs.hm.edu>
2018-11-09 12:48:54 +0100

    let Heroku set the PORT

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
